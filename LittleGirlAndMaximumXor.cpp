6aa9832fd7f38040c0873c63ba2dd543c2b032df
robinrst <31628501+robinrst@users.noreply.github.com>
2020-03-24 15:51:23 +0530

    Add files via upload

---
+++ LittleGirlAndMaximumXor.cpp
@@ -0,0 +1,83 @@
+#include        "iostream"
+#include        "algorithm"
+#include		"numeric"
+#include        "cstring"
+#include        "math.h"
+#include        "string"
+#include        "vector"
+#include        "ctime"
+#include        "queue"
+#include        "stack"
+#include        "map"
+#include        "set"
+ 
+using namespace std;
+ 
+#define          f first
+#define          lgn 25
+#define          endl '\n'
+#define          sc second
+#define          N (int)2e5+5
+#define          pb push_back
+#define          mod 1000000007
+#define          ld long double
+#define          vi vector<int>
+#define          eb emplace_back
+#define          vpii vector<pii>
+#define          mii map<int,int>
+#define          int long long 
+#define          pii pair<int,int>
+#define          pq priority_queue
+#define          test(x) while(x--)
+#define          all(x) begin(x),end(x)
+#define          allr(x) rbegin(x),rend(x)
+#define          fo(i,a,n) for(int i=a;i<n;i++)
+#define          rfo(i,n,a) for(int i=n;i>=a;i--)
+#define          db(x) cout<<#x <<" : "<< x <<endl;
+#define          FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define          time() cerr << "Time : " << (double)clock() / (double)CLOCKS_PER_SEC << "s\n"
+#define          PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286
+ 
+inline void INP()
+{
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt","r",stdin);   
+	    freopen("output.txt","w",stdout);
+	#endif 
+}
+ 
+const int inf = 0x3f3f3f3f;
+const int INF = 0x3f3f3f3f3f3f3f3f;
+ 
+void go()
+{
+	int l , r;
+ 
+	cin>>l>>r;
+ 
+	int ans = 0;
+	int shift = 0;
+ 
+	int Xor = l^r;
+ 
+	while( Xor )
+	{
+		shift++;
+		Xor >>= 1;
+	}
+ 
+	int i = 0;
+	while( shift-- ) ans += ( 1LL<<i) , i++;
+ 
+	cout << ans << endl;
+ 
+}
+ 
+int32_t main()
+{
+	INP(); 
+	FAST;     
+	int t=1; 
+	// cin>>t;
+	test(t) go();
+}
